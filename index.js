1caba63789333a0e863df1b835c5305e9fda0ce0
SkY <akash15482066@gmail.com>
2024-11-24 13:10:12 +0530

    Done 1.3

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://aws-client.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public/uploads'));
 app.use('/api/auth', authRouter);
